2312d8f28aa6d2be4e8983bdf7698a7f38813a36
unknown <shyndard@gmail.com>
2019-06-15 10:22:39 +0200

    Ajout connexion via token

--- traitement.php
+++ traitement.php
@@ -5,17 +5,43 @@
 	} catch(Exception $e) {die('Erreur : '.$e->getMessage());}
 	
 	
-	$stmt = $bdd->prepare("SELECT username FROM user WHERE username = :username AND password = :password;");
+	$stmt = $bdd->prepare("SELECT userid FROM user WHERE username = :username AND password = :password;");
 	$stmt -> bindParam(':username', $_POST["username"]);
 	$stmt -> bindParam(':password', $_POST["password"]);
 	$connected = false;
 	
-	if($stmt->execute()) {
+	
+	if(!empty($_COOKIE["token"])) {
+		$stmtVerifToken = $bdd->prepare("SELECT userid FROM user WHERE token = :token;");
+		$stmtVerifToken -> bindParam(':token', $_COOKIE["token"]);
+		
+		if($stmtVerifToken->execute()) {
+			$verifTokenRow = $stmtVerifToken->fetch();
+			
+			if(!empty($verifTokenRow["userid"])) {
+				$connected = true;
+			}
+		}
+		
+	} else if($stmt->execute()) {
 		$row = $stmt->fetch();
-		if(!empty($row["username"])) {
+		if(!empty($row["userid"])) {
+			
+			$token = uniqid();
+			
+			setcookie("token", $token);
+			
+			printf($token);
+			
+			$stmt = $bdd->prepare("UPDATE user SET token = :token WHERE userid = :userid");
+			$stmt -> bindParam(':userid', $row["userid"]);
+			$stmt -> bindParam(':token', $token);
+			$stmt->execute();
+			
 			$connected = true;
 		}
 	}
+	
 			
 	if($connected) {
 ?>
