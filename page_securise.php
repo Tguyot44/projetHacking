faaad036f8ae4c9b0259dbfdbc7cd39830b42e3b
unknown <shyndard@gmail.com>
2019-06-15 11:47:55 +0200

    ajout affichage utilisateur

--- page_securise.php
+++ page_securise.php
@@ -3,13 +3,8 @@
 		$bdd = new PDO('mysql:host=127.0.0.1;dbname=tpxss', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch(Exception $e) {die('Erreur : '.$e->getMessage());}
-	
-	
-	$stmt = $bdd->prepare("SELECT userid FROM user WHERE username = :username AND password = :password;");
-	$stmt -> bindParam(':username', $_POST["username"]);
-	$stmt -> bindParam(':password', $_POST["password"]);
+
 	$connected = false;
-	
 	
 	if(!empty($_COOKIE["token"])) {
 		$stmtVerifToken = $bdd->prepare("SELECT userid FROM user WHERE token = :token;");
@@ -29,27 +24,38 @@
 		}
 		
 	}
-	if(!$connected && $stmt->execute()) {
-		$row = $stmt->fetch();
-		if(!empty($row["userid"])) {
-			
-			$token = uniqid();
-			
-			setcookie("token", $token);
-			
-			printf($token);
-			
-			$stmt = $bdd->prepare("UPDATE user SET token = :token WHERE userid = :userid");
-			$stmt -> bindParam(':userid', $row["userid"]);
-			$stmt -> bindParam(':token', $token);
-			$stmt->execute();
-			
-			$connected = true;
+	
+	if(!$connected) {
+		$stmt = $bdd->prepare("SELECT userid FROM user WHERE username = :username AND password = :password;");
+		if($stmt->execute()) {
+			$row = $stmt->fetch();
+			if(!empty($row["userid"])) {
+				
+				$token = uniqid();
+				
+				setcookie("token", $token);
+				
+				printf($token);
+				
+				$stmt = $bdd->prepare("UPDATE user SET token = :token WHERE userid = :userid");
+				$stmt -> bindParam(':userid', $row["userid"]);
+				$stmt -> bindParam(':token', $token);
+				$stmt->execute();
+				
+				$connected = true;
+			}
 		}
 	}
 	
 			
 	if($connected) {
+		
+		$stmtSearchUser = $bdd->prepare("SELECT username FROM user;");
+		if($stmtSearchUser->execute()) {
+			while ($row = $stmtSearchUser->fetch()) {
+				echo $row["username"] . "<br/>";
+			}
+		}
 ?>
 	<html>
 	<head>
