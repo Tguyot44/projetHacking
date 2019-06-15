df736377d08a171555bf827c1026a335e36da984
unknown <shyndard@gmail.com>
2019-06-15 09:46:00 +0200

    ajout connexion bdd + changement index.html

--- traitement.php
+++ traitement.php
@@ -1,14 +1,18 @@
 <?php
-
+	try{
+		$bdd = new PDO('mysql:host=127.0.0.1;dbname=root', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(Exception $e) {die('Erreur : '.$e->getMessage());}
+	
 	//Traitement connexion ou récupération token en cookie
-	if(true) {
+	if(false) {
 ?>
 	<h1>Super tu es connecté !</h1>
 <?
 	} else {
 ?>
 	<h1>Nop, cherche pas</h1>
-	<a href="formulaire.html">Retour</a>
+	<a href="index.html">Retour</a>
 <?
 	}
 	
