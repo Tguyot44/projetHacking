e05ee84c38e55a6714931c671fb666afce9e5521
unknown <shyndard@gmail.com>
2019-06-15 09:39:59 +0200

    archi

---
+++ traitement.php
@@ -0,0 +1,15 @@
+<?php
+
+	//Traitement connexion ou récupération token en cookie
+	if(true) {
+?>
+	<h1>Super tu es connecté !</h1>
+<?
+	} else {
+?>
+	<h1>Nop, cherche pas</h1>
+	<a href="formulaire.html">Retour</a>
+<?
+	}
+	
+?>
